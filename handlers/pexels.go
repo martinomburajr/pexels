607cef604692209f4b274063770f3388a5a629b4
Martin Ombura <martin.omburajr@gmail.com>
2019-04-20 18:18:38 +0200

    added ability to view sizes upon HTTP request

--- handlers/pexels.go
+++ handlers/pexels.go
@@ -79,3 +79,18 @@ func GetRandomHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(resp))
 }
+
+//GetSizesHandler returns information about all supported sizes
+func GetSizesHandler(w http.ResponseWriter, r *http.Request) {
+	response := fmt.Sprint("\nOriginal - The size of the original image is given with the attributes width and height.\n" +
+		"Large - This image has a maximum width of 940px and a maximum height of 650px. It has the aspect ratio of the original image.\n" +
+		"Large2x - This image has a maximum width of 1880px and a maximum height of 1300px. It has the aspect ratio of the original image.\n" +
+		"Medium - This image has a height of 350px and a flexible width. It has the aspect ratio of the original image.\n" +
+		"Small - This image has a height of 130px and a flexible width. It has the aspect ratio of the original image.\n" +
+		"Portrait - This image has a width of 800px and a height of 1200px.\n" +
+		"Landscape - This image has a width of 1200px and height of 627px.\n" +
+		"Tiny - This image has a width of 280px and height of 200px.\n")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(response))
+}
